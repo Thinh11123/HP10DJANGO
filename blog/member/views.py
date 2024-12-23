fd4ec35147576f0b92f2cb232c78e35f97218ce1
willfunny <thienducbaoluu@gmail.com>
2024-12-23 19:57:44 +0700

    commit

--- blog/member/views.py
+++ blog/member/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 # def login(request):
 #     return render(request, 'login.html')
+
+
+#hehhe
+# Create your views here.
+# def login(request):
+#     return render(request, 'login.html')
